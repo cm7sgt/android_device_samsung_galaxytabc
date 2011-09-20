ca4f6c5471307701ebb4b9e02b5393db22f7ae78
technomancer <dominikbehr@yahoo.com>
2011-09-20 00:13:27 -0700

    move common stuff to p1-common/common.mk
    Change-Id: I7d5dff365868dbad3363e7f72dc5619bf7e916ba

--- device_galaxytabc.mk
+++ device_galaxytabc.mk
@@ -21,28 +21,15 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-## (1) First, the most specific values, i.e. the aspects that are specific to GSM
-
-## (2) Also get non-open-source GSM-specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/SPH-P100/SPH-P100-vendor.mk)
 
-## (3) Finally, the least specific parts, i.e. the non-GSM-specific aspects
+$(call inherit-product-if-exists, device/samsung/p1-common/common.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=200 \
-    ro.sf.hwrotation=90 \
     rild.libpath=/system/lib/libsec-ril40.so \
     rild.libargs==-d[SPACE]/dev/ttyS0 \
-    ro.telephony.ril_class=samsung \
-    phone.ril.classname=com.android.internal.telephony.SamsungRIL \
-    wifi.interface=eth0 \
-    wifi.supplicant_scan_interval=20 \
-    ro.com.google.locationfeatures=1 \
-    ro.com.google.networklocation=1 \
-    ro.setupwizard.enable_bypass=1 \
     keyguard.no_require_sim=true
 
 # for sprint
@@ -59,75 +46,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 #    ro.cdma.home.operator.alpha=VzW \
 #    ro.cdma.home.operator.numeric=310012
 
-# The OpenGL ES API level that is natively supported by this device.
-# This is a 16.16 fixed point number
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=131072
-
-# we have enough storage space to hold precise GC data
-PRODUCT_TAGS += dalvik.gc.type-precise
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapsize=48m
-
-# Extended JNI checks
-# The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
-# before they have a chance to cause problems.
-# Default=true for development builds, set by android buildsystem.
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kernel.android.checkjni=0 \
-    dalvik.vm.checkjni=false
-
-# Galaxy S uses high-density artwork where available
-PRODUCT_LOCALES += hdpi
-
 # For mobiledatainterfaces
 PRODUCT_PROPERTY_OVERRIDES += \
     mobiledata.interfaces = eth0,ppp0
 
-# For GPS
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.gps.soket = /data/gpspipe
-
-# For FM-Radio
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    fmradio.device = fmradio
-
-DEVICE_PACKAGE_OVERLAYS += device/samsung/galaxytab/overlay
-
-# These are the OpenMAX IL configuration files
-PRODUCT_COPY_FILES += \
-	device/samsung/aries-common/sec_mm/sec_omx/sec_omx_core/secomxregistry:system/etc/secomxregistry \
-	devuce/samsung/p1-common/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml
-
-# These are the OpenMAX IL modules
 PRODUCT_PACKAGES += \
-	libSEC_OMX_Core.aries \
-	libOMX.SEC.AVC.Decoder.aries \
-	libOMX.SEC.M4V.Decoder.aries \
-	libOMX.SEC.M4V.Encoder.aries \
-	libOMX.SEC.AVC.Encoder.aries \
-	libstagefrighthw \
-	overlay.SPH-P100
-
-# Install the features available on this device.
-PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
+    overlay.SPH-P100 \
+    lights.SPH-P100
 
 PRODUCT_COPY_FILES += \
-    devuce/samsung/p1-common/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
-    $(LOCAL_PATH)/prebuilt/etc/cdma-carriers-conf.xml:system/etc/cdma-carriers-conf.xml \
-    devuce/samsung/p1-common/prebuilt/usr/keylayout/p1_keyboard.kl:system/usr/keylayout/p1_keyboard.kl \
-    devuce/samsung/p1-common/prebuilt/usr/keylayout/AT42QT602240_Touchscreen.kl:system/usr/keylayout/AT42QT602240_Touchscreen.kl
+    $(LOCAL_PATH)/prebuilt/etc/cdma-carriers-conf.xml:system/etc/cdma-carriers-conf.xml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/initramfs/init.smdkc110.rc:root/init.smdkc110.rc \
@@ -136,7 +65,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/initramfs/ueventd.rc:root/ueventd.rc \
     $(LOCAL_PATH)/initramfs/recovery.rc:recovery/root/recovery.rc
 
-
 # GSM APN list override one in common_full
 PRODUCT_COPY_FILES += \
     vendor/cyanogen/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
@@ -144,18 +72,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/modelid_cfg.sh:system/bin/modelid_cfg.sh \
     $(LOCAL_PATH)/verify_cache_partition_size.sh:system/bin/verify_cache_partition_size.sh
-
-
-# Filesystem management tools
-PRODUCT_PACKAGES += \
-    setup_fs
-
-PRODUCT_COPY_FILES += \
-    devuce/samsung/p1-common/prebuilt/mke2fs:utilities/mke2fs
-
-PRODUCT_PACKAGES += \
-    libaudio \
-    lights.SPH-P100
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/samsung/galaxytabc/kernel
@@ -176,7 +92,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/modules/scsi_wait_scan.ko:root/lib/modules/scsi_wait_scan.ko \
     $(LOCAL_PATH)/prebuilt/modules/vibrator.ko:root/lib/modules/vibrator.ko \
     $(LOCAL_PATH)/prebuilt/modules/bthid.ko:root/lib/modules/bthid.ko
-
 
 $(call inherit-product, build/target/product/full.mk)
 
